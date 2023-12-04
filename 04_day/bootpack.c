2c704eb9f0ce6d31ba70a6f94a426544f706fb8a
Ayato Hayashi <39038230+hayashi-ay@users.noreply.github.com>
2023-12-05 01:10:21 +0900

    day4-3

--- 04_day/bootpack.c
+++ 04_day/bootpack.c
@@ -1,12 +1,13 @@
 void _io_hlt();
-void _write_mem8(int addr, int data);
 
 void HariMain(void)
 {
 	int i;
+	char *p;
 
 	for (i = 0xa0000; i <= 0xaffff; i++) {
-		_write_mem8(i, 15); // MOV BYTE [i], 15
+		p = (char *)i;
+		*p = i & 0x0f;
 	}
 
 	for (;;) {
