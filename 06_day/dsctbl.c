b132edf0d802f4517fc9383d24027dae28a7a678
Ayato Hayashi <39038230+hayashi-ay@users.noreply.github.com>
2023-12-05 21:36:13 +0900

    a

--- 06_day/dsctbl.c
+++ 06_day/dsctbl.c
@@ -11,20 +11,20 @@ void init_gdtidt(void)
 		set_segmdesc(gdt + i, 0, 0, 0);
 	}
 
-	set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
-	set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
-	_load_gdtr(0xffff, 0x00270000);
+	set_segmdesc(gdt + 1, 0xffffffff,   0x00000000, AR_DATA32_RW);
+	set_segmdesc(gdt + 2, LIMIT_BOTPAK, ADR_BOTPAK, AR_CODE32_ER);
+	_load_gdtr(LIMIT_GDT, ADR_GDT);
 
 	// IDTの初期化
 	for (i = 0; i < 256; i++) {
 		set_gatedesc(idt + i, 0, 0, 0);
 	}
-	_load_idtr(0x7ff, 0x0026f800);
+	_load_idtr(LIMIT_IDT, ADR_IDT);
 
 	/* IDTの設定 */
-	set_gatedesc(idt + 0x21, (int) _inthandler21, 2 * 8, AR_INTGATE32);
-	set_gatedesc(idt + 0x27, (int) _inthandler27, 2 * 8, AR_INTGATE32);
-	set_gatedesc(idt + 0x2c, (int) _inthandler2c, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x21, (int) _asm_inthandler21, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x27, (int) _asm_inthandler27, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x2c, (int) _asm_inthandler2c, 2 * 8, AR_INTGATE32);
 
 	return;
 }
