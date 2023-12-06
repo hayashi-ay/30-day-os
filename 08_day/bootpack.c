3b36a78f2b70a5859cc98106f2f70eedc03548ac
Ayato Hayashi <39038230+hayashi-ay@users.noreply.github.com>
2023-12-06 23:41:37 +0900

    day8-1

--- 08_day/bootpack.c
+++ 08_day/bootpack.c
@@ -14,6 +14,7 @@ void HariMain(void)
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
+	unsigned char mouse_dbuf[3], mouse_phase;
 
 	init_gdtidt();
 	init_pic();
@@ -34,6 +35,7 @@ void HariMain(void)
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 
 	enable_mouse();
+	mouse_phase = 0; // マウスの0xfaを待っているフェーズ
 
 	for (;;) {
 		_io_cli(); // 割り込み禁止
@@ -49,8 +51,21 @@ void HariMain(void)
 			} else if (fifo8_status(&mouseinfo) != 0) {
 				i = fifo8_get(&mouseinfo);
 				_io_sti();
-				sprintf(s, "%x", i);
-				boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 32, 16, 47, 31);
+				if (mouse_phase == 0 && i == 0xfa) {
+					mouse_phase = 1;
+				} else if (mouse_phase == 1) {
+					mouse_dbuf[0] = i;
+					mouse_phase = 2;
+				} else if (mouse_phase == 2) {
+					mouse_dbuf[1] = i;
+					mouse_phase = 3;
+				} else if (mouse_phase == 3) {
+					mouse_dbuf[2] = i;
+					mouse_phase = 1;
+					// 3バイトが揃ったので表示する
+				}
+				sprintf(s, "%x %x %x", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 32, 16, 32 + 8 * 8 -1, 31);
 				putfont8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
 			}
 		}
