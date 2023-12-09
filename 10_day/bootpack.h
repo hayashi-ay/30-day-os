4a6610bdbbb65db75fe38b95742f008359cd2fd8
Ayato Hayashi <39038230+hayashi-ay@users.noreply.github.com>
2023-12-09 14:43:11 +0900

    refactor

--- 10_day/bootpack.h
+++ 10_day/bootpack.h
@@ -112,3 +112,23 @@ void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf);
 int fifo8_put(struct FIFO8 *fifo, unsigned char data);
 int fifo8_get(struct FIFO8 *fifo);
 int fifo8_status(struct FIFO8 *fifo);
+
+// memory.c
+#define MEMMAN_FREES 4090 /* これで約32KB */
+#define MEMMAN_ADDR 0x003c0000
+
+struct FREEINFO
+{ /* あき情報 */
+	unsigned int addr, size;
+};
+
+struct MEMMAN
+{ /* メモリ管理 */
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
