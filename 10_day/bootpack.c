b52fea9a34395b4dae158b8baadf32222a7492cf
Ayato Hayashi <39038230+hayashi-ay@users.noreply.github.com>
2023-12-09 15:44:49 +0900

    tmp

--- 10_day/bootpack.c
+++ 10_day/bootpack.c
@@ -30,6 +30,9 @@ void HariMain(void)
 	struct MOUSE_DEC mdec;
 	unsigned int memtotal;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	struct SHTCTL *shtctl;
+	struct SHEET *sht_back, *sht_mouse;
+	unsigned char *buf_back, buf_mouse[256];
 
 	init_gdtidt();
 	init_pic();
@@ -40,24 +43,33 @@ void HariMain(void)
 	_io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
 
 	init_keyboard();
-
-	init_palette();
-	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
-
-	mx = (binfo->scrnx - 16) / 2; // 画面中央になるように座標計算
-	my = (binfo->scrny - 28 - 16) / 2;
-	init_mouse_cursor8(mcursor, COL8_008484);
-	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
-
 	enable_mouse(&mdec);
-
 	memtotal = memtest(0x00400000, 0xbfffffff);
 	memman_init(memman);
 	// memman_freeの2行があまり分かっていない
 	memman_free(memman, 0x00001000, 0x0009e000); /* 0x00001000 - 0x0009efff */
 	memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
+	init_palette();
+	shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+	sht_back = sheet_alloc(shtctl);
+	sht_mouse = sheet_alloc(shtctl);
+	buf_back = (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
+	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+	init_mouse_cursor8(mcursor, COL8_008484);
+	sheet_slide(shtctl, sht_back, 0, 0);
+	mx = (binfo->scrnx - 16) / 2; // 画面中央になるように座標計算
+	my = (binfo->scrny - 28 - 16) / 2;
+	sheet_slide(shtctl, sht_mouse, mx, my);
+	sheet_updown(shtctl, sht_back, 0);
+	sheet_updown(shtctl, sht_mouse, 1);
+
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);	
 	sprintf(s, "memory %dMB free: %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
 	putfont8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+	sheet_refresh(shtctl);
 
 	for (;;)
 	{
@@ -75,6 +87,7 @@ void HariMain(void)
 				sprintf(s, "%x", i);
 				boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 16, 15, 31);
 				putfont8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+				sheet_refresh(shtctl);
 			}
 			else if (fifo8_status(&mouseinfo) != 0)
 			{
@@ -107,6 +120,7 @@ void HariMain(void)
 						my = binfo->scrny - 16;
 					}
 					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* マウス描く */
+					sheet_refresh(shtctl);
 				}
 			}
 		}
--- 10_day/bootpack.h
+++ 10_day/bootpack.h
@@ -134,3 +134,12 @@ unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
 int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
 int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
+// sheet.c
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
+struct SHEET *sheet_alloc(struct SHTCTL *ctl);
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
+void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height);
+void sheet_refresh(struct SHTCTL *ctl);
+void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0);
+void sheet_free(struct SHTCTL *ctl, struct SHEET *sht);
