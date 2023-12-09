5284fa75399af4ce674c77d76873917d5d232033
Ayato Hayashi <39038230+hayashi-ay@users.noreply.github.com>
2023-12-09 16:40:22 +0900

    fix

--- 10_day/bootpack.c
+++ 10_day/bootpack.c
@@ -19,7 +19,7 @@ void HariMain(void)
 	char *vram;
 	int xsize, ysize;
 	struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
-	char s[40], mcursor[256], keybuf[32], mousebuf[128];
+	char s[40], keybuf[32], mousebuf[128];
 	int mx, my, i;
 	struct MOUSE_DEC mdec;
 	unsigned int memtotal;
@@ -50,17 +50,15 @@ void HariMain(void)
 	sht_mouse = sheet_alloc(shtctl);
 	buf_back = (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
 	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
-	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, COL8_008484);
 	init_screen8(buf_back, binfo->scrnx, binfo->scrny);
-	init_mouse_cursor8(mcursor, COL8_008484);
+	init_mouse_cursor8(buf_mouse, COL8_008484);
 	sheet_slide(shtctl, sht_back, 0, 0);
 	mx = (binfo->scrnx - 16) / 2; // 画面中央になるように座標計算
 	my = (binfo->scrny - 28 - 16) / 2;
 	sheet_slide(shtctl, sht_mouse, mx, my);
 	sheet_updown(shtctl, sht_back, 0);
 	sheet_updown(shtctl, sht_mouse, 1);
-
-	putblock8_8(buf_back, binfo->scrnx, 16, 16, mx, my, mcursor, 16);	
 	sprintf(s, "memory %dMB free: %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
 	putfont8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 	sheet_refresh(shtctl);
@@ -94,7 +92,6 @@ void HariMain(void)
 					putfont8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
 
 					/* マウスカーソルの移動 */
-					boxfill8(buf_back, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15); /* マウス消す */
 					mx += mdec.x;
 					my += mdec.y;
 					if (mx < 0)
@@ -113,8 +110,7 @@ void HariMain(void)
 					{
 						my = binfo->scrny - 16;
 					}
-					putblock8_8(buf_back, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* マウス描く */
-					sheet_refresh(shtctl);
+					sheet_slide(shtctl, sht_mouse, mx, my);
 				}
 			}
 		}
