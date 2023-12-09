e32f07febc1f1908995bd3721ae29f8883ddbbcb
Ayato Hayashi <39038230+hayashi-ay@users.noreply.github.com>
2023-12-09 15:55:18 +0900

    header

--- 10_day/bootpack.h
+++ 10_day/bootpack.h
@@ -136,6 +136,23 @@ unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
 int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 // sheet.c
+#define MAX_SHEETS 256
+#define SHEET_USE 1
+
+struct SHEET
+{
+	unsigned char *buf;
+	int bxsize, bysize, vx0, vy0, col_inv, height, flags;
+};
+
+struct SHTCTL
+{
+	unsigned char *vram;
+	int xsize, ysize, top;
+	struct SHEET *sheets[MAX_SHEETS];
+	struct SHEET sheets0[MAX_SHEETS];
+};
+
 struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
 struct SHEET *sheet_alloc(struct SHTCTL *ctl);
 void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
